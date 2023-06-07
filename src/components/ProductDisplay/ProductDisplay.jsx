cb0acdf0c563ef6f62090c1ce02110f98a751a3a
Luis Tellez <luis@luistellez.com>
2023-06-07 15:11:12 -0700

    🎨 style(ProductDisplay.jsx): format code for better readability ✨ feat(ProductDisplay.jsx): fetch products from backend API and display them dynamically 🐛 fix(ProductDisplay.jsx): use product _id instead of name as key to avoid duplicates

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./ProductDisplay.module.scss";
 import { Grid, Container, Group } from "@mantine/core";
 import ProductCard from "../ProductCard/ProductCard";
@@ -7,56 +7,37 @@ import { CartContext } from "@/contexts/CartContext";
 
 export default function ProductDisplay({ isCartOpen, toggleCart }) {
   const { cartItems, setCartItems } = useContext(CartContext);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://general-store-backend-production-62ab.up.railway.app/products"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
 
   const handleAddToCart = (product) => {
     setCartItems((currentCartItems) => [...currentCartItems, product]);
     toggleCart();
   };
 
-  const products = [
-    {
-      name: "Colombian Dark Roast",
-      image:
-        "https://cdn.shopify.com/s/files/1/0674/3411/9456/products/shop-8.png?v=1675662787&width=720",
-      price: 12.99,
-      isNew: false,
-    },
-    {
-      name: "Ethiopian Yirgacheffe",
-      image:
-        "https://cdn.shopify.com/s/files/1/0674/3411/9456/products/shop-7.png?v=1675662588&width=720",
-      price: 14.99,
-      isNew: false,
-    },
-    {
-      name: "Arabica Coffee",
-      image:
-        "https://cdn.shopify.com/s/files/1/0674/3411/9456/products/shop-4.png?v=1675661896&width=720",
-      price: 14.99,
-      isNew: false,
-    },
-    {
-      name: "Black Tea",
-      image:
-        "https://cdn.shopify.com/s/files/1/0566/9965/0230/files/img-4_720x_680bc1c2-2c6d-4b9d-81dd-55808867b5ed.png?v=1640174980",
-      price: 12.99,
-      isNew: false,
-    },
-    {
-      name: "Craft Beans",
-      image:
-        "https://dt-cafeley.myshopify.com/cdn/shop/files/specification.png?v=1665815885&width=275",
-      price: 14.99,
-      isNew: false,
-    },
-    {
-      name: "Robusta Coffee",
-      image:
-        "https://cdn.shopify.com/s/files/1/1815/2235/products/5lb_1000x.png?v=1584727129",
-      price: 14.99,
-      isNew: false,
-    },
-  ];
+  if (loading) {
+    return <div>Loading products...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching products: {error.message}</div>;
+  }
 
   return (
     <div>
@@ -67,7 +48,7 @@ export default function ProductDisplay({ isCartOpen, toggleCart }) {
         <Grid className={styles.productGrid}>
           {products.map((product) => (
             <ProductCard
-              key={product.name}
+              key={product._id}
               name={product.name}
               image={product.image}
               price={product.price}
