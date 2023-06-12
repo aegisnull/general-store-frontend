f25741875f8b1b10325244d057d821c260ae1092
Luis Tellez <luis@luistellez.com>
2023-06-12 03:54:47 -0700

    ✨ feat(admin.js): add functionality to add new products to the product list through a modal window.

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -16,6 +16,7 @@ function AdminPage() {
   const [selectedProduct, setSelectedProduct] = useState([]);
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [addModalOpen, setAddModalOpen] = useState(false);
 
   // Fetch all products from the backend on page load
   useEffect(() => {
@@ -50,6 +51,10 @@ function AdminPage() {
     setDeleteModalOpen(true);
   }
 
+  function handleAddProduct() {
+    setAddModalOpen(true);
+  }
+
   async function handleEditSubmit(event) {
     event.preventDefault();
 
@@ -82,10 +87,38 @@ function AdminPage() {
     }
   }
 
+  async function handleAddSubmit(event) {
+    event.preventDefault();
+
+    const newProduct = {
+      name: event.target.name.value,
+      image: event.target.image.value,
+      price: event.target.price.value,
+      description: event.target.description.value,
+    };
+
+    try {
+      const addedProduct = await addProduct(newProduct);
+      setProducts((prevProducts) => [...prevProducts, addedProduct]);
+      setAddModalOpen(false);
+      updateList();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div style={{ textAlign: "center" }}>
         <h2>Product Management</h2>
+        <Button
+          variant="outline"
+          color="blue"
+          style={{ marginBottom: "1rem" }}
+          onClick={handleAddProduct}
+        >
+          Add New Product
+        </Button>
         <Grid gutter="xl" cols={3} style={{ justifyContent: "center" }}>
           {products.map((product) => (
             <div key={product._id} style={{ width: "300px", margin: "1rem" }}>
@@ -201,6 +234,23 @@ function AdminPage() {
             <Text>Are you sure you want to delete {selectedProduct.name}?</Text>
             <Button onClick={handleDeleteConfirm}>Delete</Button>
           </div>
+        </Modal>
+
+        <Modal
+          title="Add Product"
+          opened={addModalOpen}
+          onClose={() => setAddModalOpen(false)}
+        >
+          <form onSubmit={handleAddSubmit}>
+            <TextInput label="Name" name="name" required />
+            <TextInput label="Image URL" name="image" required />
+            <TextInput label="Price" name="price" type="number" required />
+            <TextInput label="Description" name="description" required />
+
+            <Button type="submit" color="blue" style={{ marginTop: "1rem" }}>
+              Add Product
+            </Button>
+          </form>
         </Modal>
       </div>
     </div>
